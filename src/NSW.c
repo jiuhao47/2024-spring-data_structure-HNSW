e5d97f1303d6d4ae2e9b133ec7763dcf58519364
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2024-06-19 09:58:49 +0800

    inc add

--- src/NSW.c
+++ src/NSW.c
@@ -1,27 +1,27 @@
-include <stdio.h>
-include <stdlib.h>
-include "definition.h"
-include "structures.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/definition.h"
+#include "../inc/structures.h"
 
-//初始化HNSW
-void InitalGraph(HNSW_Graph *G){
-
+// 初始化HNSW
+void InitalGraph(HNSW_Graph *G)
+{
 }
 
-//插入新节点
-Node *InsertNode(HNSW_Graph *G, NodeDataType data){
-
+// 插入新节点
+Node *InsertNode(HNSW_Graph *G, NodeDataType data)
+{
 }
 
-//搜索m-邻近节点
-Node *Search(HNSW_Graph *G){
-
+// 搜索m-邻近节点
+Node *Search(HNSW_Graph *G)
+{
 }
 
-//随机插入层数
-int RandomLevel(){
+// 随机插入层数
+int RandomLevel()
+{
     int level;
-
 
     return level;
 }
