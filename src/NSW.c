e78ab1cc055f4f1a2a1e6688043d2da39560927b
ArminChiu <Armin_Chiu@outlook.com>
2024-06-20 21:39:58 +0800

    change in libraries and NSW functions

--- src/NSW.c
+++ src/NSW.c
@@ -19,6 +19,7 @@ Node *InsertNode(HNSW_Graph *G, NodeDataType data)
     int level = RandomLevel();
     Node *lastNode;                                              // 记录上一个节点，用于连接
     Node *newNode = (Node *)malloc(sizeof(Node));                // 最顶层节点分配空间
+    newNode->data = data;                                        // 赋值
     newNode->pList = (Node **)malloc(sizeof(Node *) * MAX_NEAR); // 分配m-邻近节点空间
     if (level > G->highestLevel)                                 // 每一层的第一个节点都作为入口点
     {
@@ -29,8 +30,19 @@ Node *InsertNode(HNSW_Graph *G, NodeDataType data)
     {
         // 给每一层的点分配空间（i不等于level时）
         // 连接上一个节点，然后更新lastNode
+        if(i !=level){
+            newNode = (Node *)malloc(sizeof(Node));
+            newNode->data = data;
+            newNode->pList = (Node **)malloc(sizeof(Node *) * MAX_NEAR);
+            lastNode->nextLevel = newNode;
+        }
+        if(i == 0){
+            newNode->nextLevel = NULL;
+        }
         // 每一层要连接m-邻近节点，调用ConnectNode函数
+        ConnectNode(G, newNode, i);
         // 注意第0层连接NULL
+        lastNode = newNode;
     }
     return newNode;
 }
@@ -38,20 +50,23 @@ Node *InsertNode(HNSW_Graph *G, NodeDataType data)
 // 连接新节点与第level层的m-邻近节点
 void ConnectNode(HNSW_Graph *G, Node *newNode, int level)
 {
+    //critical and hard!
 }
 
 // 搜索m-邻近节点
 Node *Search(HNSW_Graph *G)
 {
+    //critical and hard!
 }
 
 // 随机插入层数
 int RandomLevel()
 {
-    int level;
-
+    double u = (double)rand() / RAND_MAX;
+    int level = (int)(-log(u) / log(2.0) * RANDOMWeight);
     return level;
 }
+
 float CosineDistance(float *a, float *b, int size)
 {
     float dot = 0.0, denom_a = 0.0, denom_b = 0.0;
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 #include "definition.h"
 #include "structures.h"
 int main()
