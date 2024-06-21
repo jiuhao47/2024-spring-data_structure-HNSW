9a9c22713b5c0b24028dab93dc4ece1ffe694f2c
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2024-06-21 15:24:45 +0800

    2024-06-21

--- data/distance.py
+++ data/distance.py
@@ -11,3 +11,6 @@ def Cosine_similarity(index_x, index_y):
     x = np.loadtxt("../data/preprocess/" + str(index_x) + ".txt")
     y = np.loadtxt("../data/preprocess/" + str(index_y) + ".txt")
     return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+
+
+P
